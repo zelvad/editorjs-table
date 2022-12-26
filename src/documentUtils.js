412b9c20cd3674743143d8c4eade16c3eef1a536
Intaek Hwang <intaek.hwang@medistream.co.kr>
2022-12-26 13:41:36 +0900

    엘리먼트 생성 유틸 함수를 만든다

--- src/documentUtils.js
+++ src/documentUtils.js
@@ -34,11 +34,49 @@ export function create(tagName, cssClasses = null, attrs = null, children = null
   if (_isNotMissed(children)) {
     for (let i = 0; i < children.length; i++) {
       if (_isNotMissed(children[i])) {
-        elem.appendChild(children[i]);
+        elem.appendChild(children[i])
       }
     }
   }
-  return elem;
+  return elem
+}
+
+/**
+ *
+ * @param {Object} data
+ * @param {String} data.tag
+ * @param {String | String[] | null} data.class
+ * @param {Object | null} data.attribute
+ * @param {HTMLElement[] | HTMLElement | null} data.children
+ */
+export function createElement(data) {
+  const element = document.createElement(data.tag)
+
+  if (typeof data.class === "string") {
+    element.classList.add(data.class)
+  } else if (Array.isArray(data.class)) {
+    data.class.forEach((className) => {
+      element.classList.add(className)
+    })
+  }
+
+  if (typeof data.attribute === "object") {
+    for (let key in data.attribute) {
+      element.setAttribute(key, data.attribute[key])
+    }
+  }
+
+  if (data.children === undefined || data.children === null) {
+    return element
+  } else if (Array.isArray(data.children)) {
+    data.children.forEach((child) => {
+      element.appendChild(child)
+    })
+  } else if (data.children !== null) {
+    element.appendChild(data.children)
+  }
+
+  return element
 }
 
 /**
