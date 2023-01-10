709cd059f995fb4c93bb6200b3f084b35b7219e7
Intaek Hwang <intaek.hwang@medistream.co.kr>
2023-01-10 18:24:58 +0900

    Row, Column 추가 버튼을 삭제한다

--- src/table.js
+++ src/table.js
@@ -650,14 +650,12 @@ export class Table {
     ])
 
     if (!this.readOnly) {
-      const addRowButton = create("div", [CSS.addRowButton])
-      const addColumnButton = create("div", [CSS.addColumnButton])
-
-      addRowButton.addEventListener("click", () => this.addColumn(this._numberOfColumns), true)
-      addColumnButton.addEventListener("click", () => this.addRow(this._numberOfRows), true)
-
-      wrapper.appendChild(addRowButton)
-      wrapper.appendChild(addColumnButton)
+      // const addRowButton = create("div", [CSS.addRowButton])
+      // const addColumnButton = create("div", [CSS.addColumnButton])
+      // addRowButton.addEventListener("click", () => this.addColumn(this._numberOfColumns), true)
+      // addColumnButton.addEventListener("click", () => this.addRow(this._numberOfRows), true)
+      // wrapper.appendChild(addRowButton)
+      // wrapper.appendChild(addColumnButton)
     }
 
     return wrapper
